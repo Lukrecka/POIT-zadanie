e980deb5c86e9fc415fffd1db33fb9c3bd2690c6
Lukrecka <lukrecia.szilvasiova13@gmail.com>
2022-05-17 18:01:01 +0200

    kod pre arduino

---
+++ arduino/arduino.ino
@@ -0,0 +1,35 @@
+#define echoPin 2 // ECHO na D2
+#define trigPin 3 //TRIG na D3
+#define LedPin 6
+
+// definovanie premennych
+long duration;
+int distance; 
+int zadHod = 10;
+long m;
+void setup() {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  pinMode(LedPin, OUTPUT);
+  Serial.begin(9600); 
+
+}
+void loop() {
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2; 
+  Serial.println(distance);
+
+  if(distance >= zadHod && distance < 61){
+    m = map(distance, 60, zadHod, 0, 255);
+    
+  }
+  else{ m = 0;}
+  analogWrite(LedPin, m);
+  
+}
